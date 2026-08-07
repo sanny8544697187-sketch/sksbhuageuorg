972502de037d02807c8715fa404e6fb7be768361
Sanny Kumar <sanny8544697187@gmail.com>
2026-08-07 22:07:49 +0530

    Add TWA Android project for KrishiGyan (package: online.sannykumar.krishigyan_v1)
    - Added Bubblewrap-generated Android TWA source code
    - Package name: online.sannykumar.krishigyan_v1
    - minSdkVersion: 21, targetSdkVersion: 36
    - App launches krishigyan.online as a Trusted Web Activity
    - Added .gitignore to exclude build artifacts and signing keys

---
+++ app/src/main/java/online/sannykumar/krishigyan_v1/DelegationService.java
@@ -0,0 +1,14 @@
+package online.sannykumar.krishigyan_v1;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
